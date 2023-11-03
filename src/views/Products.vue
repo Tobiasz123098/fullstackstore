77bb494ccb122274c2a96d5b4b651cd4dbb007cb
Tobiasz Czaprowicz <tczaprowicz@puretech.pl>
2023-11-03 17:05:32 +0100

    finished course

---
+++ src/views/Products.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+
+    <div class="card-container">
+      <ProductCard v-for="(product, index) in inventory"
+        :key="product.id"
+        :product="product"
+        :index="index"
+        :addToCart="addToCart"
+      />
+    </div>
+
+  </main>
+</template>
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+  name: 'Products',
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
